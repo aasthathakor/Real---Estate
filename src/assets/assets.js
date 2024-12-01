310bc5b57c691e4614c97b4fe1b1e7ca3ac0f7d1
Astha Thakur <128837575+aasthathakor@users.noreply.github.com>
2024-12-01 22:51:14 +0530

    First Commit

---
+++ src/assets/assets.js
@@ -0,0 +1,24 @@
+export const assets = {
+    logo,
+    logo_dark
+}
+export const projectsData = [
+    
+    {
+        title: "Skyline Haven",
+        price: "",
+        location: "California",
+        image: Apple
+    },
+];
+
+export const testimonialsData = [
+    {
+        name: "Donald Trump",
+        title: "Marketing Manager",
+        image: profile,
+        alt: "Portrait of donald Trump",
+        rating: 5,
+        text: "From the very first meeting, they understood my vision and helped me find the perfect property. their attention to details and commitment to client satisfaction is unmatched."
+    },
+]
