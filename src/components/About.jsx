8447c22d3a61002367447787c28472f080911e0d
Astha Thakur <128837575+aasthathakor@users.noreply.github.com>
2024-12-03 22:24:27 +0530

    Project slider

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { assets} from '../assets/assets'
 
 const About = () => {
   return (
@@ -34,6 +34,7 @@ const About = () => {
     <p className=' my-10 max-w-lg'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     <button className=' bg-blue-600 text-white px-8 py-2 rounded'>Learn More</button>
     </div>
+
     </div>
     </div>
   )
